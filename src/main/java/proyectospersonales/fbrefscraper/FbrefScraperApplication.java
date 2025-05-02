f834c9b6ef1af42f0efb2836afddff3084388acf
Ronaldinho-OrtegaRuiz <rortegar01@libertadores.edu.co>
2025-05-02 11:50:09 -0500

    Tercer Commit: - Obtener Estadísticas del local y del visitante para promediar.

--- src/main/java/proyectospersonales/fbrefscraper/FbrefScraperApplication.java
+++ src/main/java/proyectospersonales/fbrefscraper/FbrefScraperApplication.java
@@ -3,18 +3,31 @@ package proyectospersonales.fbrefscraper;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import proyectospersonales.fbrefscraper.analyzer.CsvStatsLoader;
+import proyectospersonales.fbrefscraper.analyzer.StatsAnalyzer;
 import proyectospersonales.fbrefscraper.scraper.LeagueStatsScraper;
 import proyectospersonales.fbrefscraper.scraper.MatchStatsScraper;
 
+import java.util.List;
+
 @SpringBootApplication
 public class FbrefScraperApplication implements CommandLineRunner {
+
     private final MatchStatsScraper matchStatsScraper;
     private final LeagueStatsScraper leagueStatsScraper;
+    private final CsvStatsLoader csvStatsLoader;
+    private final StatsAnalyzer statsAnalyzer;
 
-    public FbrefScraperApplication(MatchStatsScraper matchStatsScraper, LeagueStatsScraper leagueStatsScraper) {
+    public FbrefScraperApplication(
+            MatchStatsScraper matchStatsScraper,
+            LeagueStatsScraper leagueStatsScraper,
+            CsvStatsLoader csvStatsLoader,
+            StatsAnalyzer statsAnalyzer
+    ) {
         this.matchStatsScraper = matchStatsScraper;
         this.leagueStatsScraper = leagueStatsScraper;
-
+        this.csvStatsLoader = csvStatsLoader;
+        this.statsAnalyzer = statsAnalyzer;
     }
 
     public static void main(String[] args) {
@@ -23,14 +36,18 @@ public class FbrefScraperApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-
-        String localTeam = "Real Valladolid";
+        String localTeam = "Valladolid";
         String awayTeam = "Barcelona";
         String localLeague = "Spanish La Liga";
         String awayLeague = "Spanish La Liga";
+        String competition = "Spanish La Liga";
 
-        //leagueStatsScraper.scrape(localLeague, awayLeague);
-        matchStatsScraper.scrape(localTeam, localLeague, awayTeam, awayLeague);
+        List<double[]> localStats = csvStatsLoader.loadAllStats(localTeam, "local");
+        List<double[]> awayStats = csvStatsLoader.loadAllStats(awayTeam, "visitante");
+
+        statsAnalyzer.analyzeAndPrintStats(localStats, localTeam + " (local)");
+
+        statsAnalyzer.analyzeAndPrintStats(awayStats, awayTeam + " (visitante)");
 
     }
 }
